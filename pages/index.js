c32ab54e30f458b106082c7a51f3f1d82f7901cc
LeandroMileski <leandrotinoco55@gmail.com>
2025-04-15 15:01:33 +0000

    change message in `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,49 @@
 function Home() {
-    const containerStyle = {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        backgroundColor: '#f0f8ff',
-        fontFamily: 'Arial, sans-serif',
-        textAlign: 'center',
-    };
+  const containerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    backgroundColor: "#f0f8ff",
+    fontFamily: "Arial, sans-serif",
+    textAlign: "center",
+  };
 
-    const textStyle = {
-        color: '#333',
-        fontSize: '2rem',
-        lineHeight: '1.5',
-        animation: 'fadeIn 2s ease-in, bounce 1.5s infinite',
-    };
+  const textStyle = {
+    color: "#333",
+    fontSize: "2rem",
+    lineHeight: "1.5",
+    animation: "fadeIn 2s ease-in, bounce 1.5s infinite",
+  };
 
-    return (
-        <div style={containerStyle}>
-            <h1 style={textStyle}>
-                Gabi, eu amo você. <br />
-                Se você me ama, dá uma risadinha! 😎
-            </h1>
-            <style jsx>{`
-                @keyframes fadeIn {
-                    from {
-                        opacity: 0;
-                    }
-                    to {
-                        opacity: 1;
-                    }
-                }
+  return (
+    <div style={containerStyle}>
+      <h1 style={textStyle}>
+        Frota team rumo a <br />
+        Bariloche!🏂🍻😎
+      </h1>
+      <style jsx>{`
+        @keyframes fadeIn {
+          from {
+            opacity: 0;
+          }
+          to {
+            opacity: 1;
+          }
+        }
 
-                @keyframes bounce {
-                    0%, 100% {
-                        transform: translateY(0);
-                    }
-                    50% {
-                        transform: translateY(-10px);
-                    }
-                }
-            `}</style>
-        </div>
-    );
+        @keyframes bounce {
+          0%,
+          100% {
+            transform: translateY(0);
+          }
+          50% {
+            transform: translateY(-10px);
+          }
+        }
+      `}</style>
+    </div>
+  );
 }
 
 export default Home;
